fff559c31932cbafb82ae17c9c4c767447b87c33
hafza-mughal <156007447+hafza-mughal@users.noreply.github.com>
2024-04-08 20:18:29 +0500

    Add files via upload

---
+++ jaava.js
@@ -0,0 +1,6 @@
+let FirstName = prompt("Enter Your First Name")
+let middlename = prompt("Enter your middle name")
+let LastName = prompt("Enter Your Last Name")
+
+
+console.log(FirstName,middlename, LastName);
