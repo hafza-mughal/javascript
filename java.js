1e8f814f58b57acf1529c195e770cb8b6d6a004c
hafza-mughal <156007447+hafza-mughal@users.noreply.github.com>
2024-04-26 07:43:15 -0700

    java assignment 2  is uploaded

--- java.js
+++ java.js
@@ -1,62 +1,10 @@
 
 
+let num1= prompt("enter the num")
+let num2= prompt("enter the num")
+let num3= prompt("enter the num")
 
 
-
-
-let percentage=prompt(" Enter your percentage");
-
-
-
-if(percentage >=90 && percentage <=100 )
-{
-        console.log("Grade A+1")
-          
-        
-        Grade = "A+1"
-}
-else if (percentage >=80 && percentage <=89 )
-{
-    console.log("Grade A1")
-      
-    
-    Grade = "A1"
-}
-else if (percentage >=70 && percentage <=79 ) 
-{
-    console.log("Grade A")
-   
-   
-    Grade = "A"
-}
-else if (percentage >=60 && percentage <=69 )
-{
-    console.log("Grade B")
-    
-    
-    Grade = "B"
-}
-else if (percentage >=50 && percentage <=59 )
-{
-    console.log("Grade C")
-         
-    
-    Grade = "C"
-
-}
-else if (percentage >= 40 && percentage <= 49)
-{
-    console.log("Grade D");
-   
-   
-     Grade = "D"
-}
-else if (percentage <39)
- {
-    console.log("Fail");
-  
-  
-    Grade = "Fail"
-
-}
+console.log("the result of adding three num is :" ,+num1+ +num2+
+ +num3)
 
